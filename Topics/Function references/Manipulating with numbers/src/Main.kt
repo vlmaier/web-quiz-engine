bc83ac66aa8f3c2ab402c57a450fc76a94c1ae36
Vladas Maier <vladas.maier@goyahealth.de>
2023-05-27 13:08:28 +0200

    Solve 'Function references'

---
+++ Topics/Function references/Manipulating with numbers/src/Main.kt
@@ -0,0 +1,26 @@
+fun multiplyByTwo(number: Int): Int {
+    return number * 2
+}
+
+fun addTen(number: Int): Int {
+    return number + 10
+}
+
+fun changeNumber(changeFunction: (Int) -> Int, number: Int) {
+    val newNumber = changeFunction(number)
+    print("$newNumber ")
+}
+
+fun main() {
+    val numbers = readln().split(' ').map { it.toInt() }.toList()
+    var numberFun: (Int) -> Int
+    for (number in numbers) {
+        // write your code here
+        numberFun = if (number % 2 == 0) {
+            ::addTen
+        } else {
+            ::multiplyByTwo
+        }
+        changeNumber(numberFun, number)
+    }
+}
